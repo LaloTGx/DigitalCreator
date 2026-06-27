0253e0bfe174c197907d2511a3561441fd81b500
LaloTGx <89763322+LaloTGx@users.noreply.github.com>
2026-06-27 11:32:31 -0600

    Change path env script, add comments for use
    Cambie la ruta del entorno para que sea mas facil de ejecutar tambien
    agregue comentarios de ayuda para que sea mas facil su configuración :)

--- PyScripts/Notifymsgtwt.py
+++ PyScripts/Notifymsgtwt.py
@@ -1,15 +1,17 @@
-#!/home/user/repositorios/PyCreators/venv/bin/python
+#!/usr/bin/env python3
 import socket
 import os
 
+# Tienes que crear un env dentro del repositorio para poder ejecutar el script
+# Comando: python -m venv venv
 current_pid = os.getpid()
 os.system(f"pgrep -f Notifymsgtwt.py | grep -v {current_pid} | xargs kill -9 2>/dev/null")
 
 server = 'irc.chat.twitch.tv'
 port = 6667
-nickname = 'User'
-token = 'oauth:'
-channel = '#user'
+nickname = 'User'# Nombre de usuario con mayusculas y minusculas
+token = 'oauth:' # Token para recibir los mensajes, lo puedes generar aqui: https://twitchtokengenerator.com/
+channel = '#user'# Canal de twitch en minusculas (donde recibira los mensajes)
 
 sock = socket.socket()
 sock.connect((server, port))
